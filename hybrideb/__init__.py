785a51fe9834ee6800523e27b8474f9603dfe088
Matthew R. Becker <becker.mr@gmail.com>
2022-02-02 15:56:09 -0600

    ENH fix setup

--- hybrideb/__init__.py
+++ hybrideb/__init__.py
@@ -1,3 +1,14 @@
 # flake8: noqa
+try:
+    from importlib.metadata import version, PackageNotFoundError
+except ImportError:
+    from importlib_metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version("pizza_cutter_metadetect")
+except PackageNotFoundError:
+    # package is not installed
+    pass
+
 from ._hybrideb import HybridEB, GaussEB, SimpleGaussEB
 from ._bineb import BinEB
